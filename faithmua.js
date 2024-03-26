c26b1d099021f9aa53a6577e1adbbac5a7b51fc7
muafaith <161202336+muafaith@users.noreply.github.com>
2024-03-26 17:45:50 +0300

    faithmua.js

--- faithmua.js
+++ faithmua.js
@@ -5,3 +5,14 @@ function isEven(number){
 let number = 7;
 let result = isEven(number);
 console.log(result);
+
+
+
+function isEven(number){
+    return number % 2 ===0;
+}
+
+let number = 12;
+let result = isEven(number);
+console.log(result);
+
